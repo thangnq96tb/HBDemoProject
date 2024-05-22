ff0e65e9ad1ea4b49251afc8576df6e06ce05261
Nguyễn Quang Thắng <thangnq96tb@gmail.com>
2024-05-22 13:50:18 +0700

    *complete copy game part 1
    - control player: moving, jumping, attacking
    - change player's animation

---
+++ game/Assets/_Game/Scripts/SCR_Player.cs
@@ -0,0 +1,167 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator anim;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float speed = 5;
+    [SerializeField] private float jumpForce = 350;
+
+    private bool isGrounded = true;
+    private bool isJumping = false;
+    private bool isAttack = false;
+
+    private float horizontal;
+
+    private string currentAnimName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = CheckGrounded();
+
+        //-1: left; 1: right; 0: nothing... (keyboard) 
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if(isAttack)
+        {
+            rb.velocity = Vector2.zero;
+            return;
+        }    
+
+        if (isGrounded)
+        {
+            if (isJumping)
+            {
+                return; //do nothing when jumping
+            }
+
+            //jump
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                Jump();
+            }
+
+            //change anim run
+            if (Mathf.Abs(horizontal) > 0.1f)
+            {
+                ChangeAnim("run");
+            }
+
+            //attack
+            if (Input.GetKeyDown(KeyCode.C) && isGrounded)
+            {
+                Attack();
+            }
+
+            //throw
+            if (Input.GetKeyDown(KeyCode.V) && isGrounded)
+            {
+                Throw();
+            }
+        }
+
+        //check falling
+        if (!isGrounded && rb.velocity.y < 0)
+        {
+            isJumping = false;
+            ChangeAnim("fall");
+        }
+
+        //moving
+        if (Mathf.Abs(horizontal) > 0.1f)
+        {
+            rb.velocity = new Vector2(horizontal * Time.fixedDeltaTime * speed, rb.velocity.y); //using velocity.y because player still falling when moving in air (not ground)
+            //transform.localPosition = new Vector3(horizontal, 1, 1); //not option, using rotation to flip
+            transform.rotation = Quaternion.Euler(new Vector3(0, horizontal > 0 ? 0 : 180, 0));
+        }
+        else if (isGrounded)
+        {
+            ChangeAnim("idle");
+            rb.velocity = Vector2.zero;
+        }
+
+        ////jump
+        //if (isGrounded && rb.velocity.y < 0)
+        //{
+        //    ChangeAnim("fall");
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        //{
+        //    //isJumping = true;
+        //    ChangeAnim("jump");
+        //    rb.AddForce(jumpForce * Vector2.up);
+        //}
+
+        ////moving
+        //if (Mathf.Abs(horizontal) > 0.1f)
+        //{
+        //    ChangeAnim("run");
+        //    rb.velocity = new Vector2(horizontal * Time.fixedDeltaTime * speed, rb.velocity.y); //using velocity.y because player still falling when moving in air (not ground)
+
+        //    //not optimize
+        //    //transform.localPosition = new Vector3(horizontal, 1, 1);
+        //    transform.rotation = Quaternion.Euler(new Vector3(0, horizontal > 0 ? 0 : 180, 0));
+        //}
+        //else if (isGrounded)
+        //{
+        //    ChangeAnim("idle");
+        //    rb.velocity = Vector2.zero;
+        //}
+    }
+
+    private bool CheckGrounded()
+    {
+        //Debug.DrawLine(transform.position, transform.position + Vector3.down * 1.1f, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayer);
+        return hit.collider != null;
+    }
+
+    private void Attack()
+    {
+        isAttack = true;
+        rb.velocity = Vector2.zero;
+        ChangeAnim("attack");
+        Invoke(nameof(ResetAttack), 0.5f);
+    }   
+    
+    private void Throw()
+    {
+        isAttack = true;
+        rb.velocity = Vector2.zero;
+        ChangeAnim("throw");
+        Invoke(nameof(ResetAttack), 0.5f);
+    }
+
+    private void Jump()
+    {
+        isJumping = true;
+        ChangeAnim("jump");
+        rb.AddForce(jumpForce * Vector2.up);
+    }
+
+    private void ChangeAnim(string animName)
+    {
+        if(currentAnimName != animName)
+        {
+            anim.ResetTrigger(animName);
+            currentAnimName = animName;
+            anim.SetTrigger(currentAnimName);
+        }    
+    }    
+
+    private void ResetAttack()
+    {
+        isAttack = false;
+        ChangeAnim("ilde");
+    }    
+}
